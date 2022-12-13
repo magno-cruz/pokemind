7f06ff91bac4d4a2c257fed48136a45af258f839
Magno <magnocruz@proton.me>
2022-12-12 22:59:40 -0300

    Começando a botar a descrição da Pokedex

--- api.js
+++ api.js
@@ -1,61 +1,25 @@
-// let number = 303
-// const endpoint = new URL(`https://pokeapi.co/api/v2/pokemon/${number}`);
-// //console.log(endpoint);
-
-// async () => { // with Async/Await
-//      try {
-//          const poke = await fetch(`https://pokeapi.co/api/v2/pokemon/${number}`);
-// //         // const frenchName = golduckSpecies.names.filter(pokeAPIName => pokeAPIName.language.name === 'fr')[0].name;
-//          console.log(poke);
-//      } catch (error) {
-//          throw error;
-// 		}
-// 	}
-
-
-// let pokeapi = fetch('https://pokeapi.co/api/v2/pokemon/clefairy/')
-// 				.then(res => res.json())
-// 				.then(pokemon => console.log(pokemon))
-// 				console.log(pokemon.id)
-
-
-// //console.log(pokeapi)
-// //let pokemon = JSON.parse(pokeapi)
-// //console.log(pokemon)
-// //JSON.parse(api)[0].sprites/other/official-artwork/front_default :
-
-// P.getPokemonByName(['eevee', 'ditto'])
-// 	.then((response) => {
-// 		console.log(response);
-// 	})
-// 	.catch((error) => {
-// 		console.log('There was an ERROR: ', error);
-// 	})
-
-// async function nomeFuncao() {
-
-// }
-
-
-
-// const response = await fetch(endpoint);
-
-// const data = await response.json();
-
-// alert(`Country: ${data.country}\nCapital: ${data.capital || "NA"}\nInternet Code: ${data.internetcode}`);
-
 function fetchPokemon(id) {
 	fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
 	.then(Response => Response.json())
 	.then(pokemon => {
-		console.log(pokemon);
-		teste = pokemon.sprites.other.home.front_default;
-		console.log(teste)});	
+		teste = pokemon.sprites.front_default;
+	console.log(pokemon);
+})}
+
+
+fetchPokemon(303)
+//pokemon = fetchPokemon(303);
+
+function funcaoteste() {
+	let id = Math.floor(Math.random() * 152);
+
+	let next = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
+
+	document.getElementById("picture").src = next;
 }
 
+// let picture = document.getElementById("picture").src;
+// picture = "teste";
+// console.log (picture);
 
-pokemon = fetchPokemon(303);
-pokemon
-console.log(pokemon);
 
-//console.log(pokemon)
