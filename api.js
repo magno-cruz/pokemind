6f924ec2e0a0bca9ef10c777ad83ff3f60e71ad5
Magno <magnocruz@proton.me>
2022-12-15 21:07:25 -0300

    Dor de cabeça pra pegar o valor do fetch

--- api.js
+++ api.js
@@ -1,22 +1,32 @@
-function fetchPokemon(id) {
-	fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
-	.then(Response => Response.json())
-	.then(pokemon => {
-		teste = pokemon.sprites.front_default;
-	console.log(pokemon);
-})}
+// async function pokename() {
+// 	const response = await fetch(`https://pokeapi.co/api/v2/pokemon/303/`);
+// 	const data = await data.json()
+// 	return pokemon.name;
+// }
 
 
-fetchPokemon(303)
-//pokemon = fetchPokemon(303);
-
-function funcaoteste() {
-	let id = Math.floor(Math.random() * 152);
-
-	let next = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
-
-	document.getElementById("picture").src = next;
+async function pokename() {
+	fetch(`https://pokeapi.co/api/v2/pokemon/303/`)
+	.then((response) => response.json())
+	.then((data) => {
+		console.log(data.name);
+	})
 }
+
+console.log(await pokename());
+
+//console.log(fetchPokemon(303));
+//poke = fetchPokemon(303);
+//console.log(typeof poke);
+
+// function next() {
+// 	let id = Math.floor(Math.random() * 152);
+// 	document.getElementById("picture").src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
+// 	pokename(id);
+// 	document.getElementById("name").innerHTML = pokename.text;
+
+
+// }
 
 // let picture = document.getElementById("picture").src;
 // picture = "teste";
