85ebd28f04c2305e4052f7f1f1cf00471a31a5ce
Magno <magnocruz@proton.me>
2022-12-16 21:46:52 -0300

    Quase resolvendo a  API, só falta selecionar o idioma correto.

--- api.js
+++ api.js
@@ -1,43 +1,50 @@
-async function pokename() {
-	const response = await fetch(`https://pokeapi.co/api/v2/pokemon/303/`);
-	const data = await response.json()
-	return data;
+async function pokename(id) {
+	const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
+	const data = await response.json();
+	displayName(data.name.toUpperCase());
 }
-console.log(pokename());
-let id = Math.floor(Math.random() * 152);
+
+async function pokedex() {
+	const response = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}/`);
+	const data = await response.json();
+	displayPokedex(data['flavor_text_entries'][language.name === 'en']['flavor_text']); 
+}
+
+
+async function obj() {
+	const response = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}/`);
+	const data = await response.json();
+	console.log(data);
+}
+
+function displayName(nome) {
+	document.getElementById("pokename").innerHTML = nome;
+}
+
+function displayPokedex(nome) {
+	document.getElementById("pokedex").innerHTML = nome;
+}
+
+let id = "78"; //Math.floor(Math.random() * 152);
 
 // function pokename() {
 // 	fetch(`https://pokeapi.co/api/v2/pokemon/303/`)
 // 	.then((response) => response.json())
 // 	.then((data) => {
-// 		return(data.name.toUpperCase());
+// 		return data.name;
 // 	})
-
 // }
 
-function pokedex() {
-	fetch(`https://pokeapi.co/api/v2/pokemon-species/303/`)
-	.then((response) => response.json())
-	.then((data) => {
-		console.log(data['flavor_text_entries'][0]['flavor_text']);
-	})
-
-}
-//console.log(pokedex());
+pokedex(id);
+pokename(id);
+obj();
 
 // function next() {
 // 	
 // 	document.getElementById("picture").src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
 // 	pokename(id);
 // 	document.getElementById("name").innerHTML = pokename.text;
-
-
 // }
 
-// let picture = document.getElementById("picture").src;
-// picture = "teste";
-// console.log (picture);
 
-//document.getElementById("pokename").innerHTML = pokename();
-//document.getElementById("pokedex").innerHTML = pokedex();
 
